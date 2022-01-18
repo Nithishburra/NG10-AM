a3a6f28ab31d28b3b52f5813968e74fb1bbd4a90
Nithish <Nithishgoudb@gmail.com>
2022-01-18 19:52:44 +0530

    code

---
+++ spaDemo1/src/app/pagethree.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pagethree',
+  templateUrl: './pagethree.component.html',
+  styles: [
+  ]
+})
+export class PagethreeComponent implements OnInit {
+
+  var_three:string = "pagethree";
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
